5c687b3c1b07f6dc298befb8a3f227456ac45e18
Wind Wu <wujiejunwei@163.com>
2017-03-26 10:21:55 +0800

    add markdown

--- myblog/settings.py
+++ myblog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'article',
+    #'markdown',
 ]
 from django.conf import global_settings
 BOOTSTRAP_ADMIN_SIDEBAR_MENU = True
